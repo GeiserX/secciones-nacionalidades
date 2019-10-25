d3eeb077eb4c38f1b1fa4d8bcb9dd1d29e23ddca
GeiserX <9169332+GeiserX@users.noreply.github.com>
2019-10-25 15:44:37 +0200

    Add feature: new tab, unfinished

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ dashboardPage(
     dashboardSidebar(
       sidebarMenu(
         menuItem("Map it", tabName = "tab1", icon = shiny::icon("building")),
-        menuItem("Statistics by Province", tabName = "tab2", icon = shiny::icon("building")),
+        menuItem("Statistics by Province", tabName = "tab2", icon = shiny::icon("brain")),
         menuItem("Historic data", tabName = "tab3", icon = shiny::icon("binoculars"))
     )
   ),
@@ -11,20 +11,32 @@ dashboardPage(
     fluidRow(
       tabItems(
         tabItem(tabName = "tab1",
-                box(width = 3, title = "Selecciona Municipio y Nacionalidad", status = "primary", solidHeader = TRUE,
-                    selectizeInput("selectProvincia", "Selecciona Provincia", choices = provincias$Nombre, multiple = F, selected = "Murcia"),
-                    selectizeInput("selectMunicipio", label = "Selecciona Municipio", choices = c(""), multiple = T),
-                    selectizeInput("selectNacionalidad", label = "Selecciona Nacionalidad", choices = levels(SXnacionalAmbos$nacionalidad)),
-                    checkboxInput("porcentaje", "Aplicar porcentaje sobre población total", value = F),
-                    checkboxInput("hombreMujer", "Distinguir entre hombre/mujer", value = F),
+                box(width = 3, title = "Select Municipality and Nationality", status = "primary", solidHeader = TRUE,
+                    selectizeInput("selectProvincia", "Select Province", choices = provincias$Nombre, multiple = F, selected = "Murcia"),
+                    selectizeInput("selectMunicipio", label = "Select Municipality", choices = c(""), multiple = T),
+                    selectizeInput("selectNacionalidad", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad)),
+                    checkboxInput("porcentaje", "Apply percentage over total population", value = F),
+                    checkboxInput("hombreMujer", "Distinguish between man/womanr", value = F),
                     useShinyjs(),
-                    disabled(downloadButton("descargaKML", label = "Descargar seleccionados")),
+                    disabled(downloadButton("descargaKML", label = "Download selected (blue) areas")),
                     htmlOutput("mention")
                 ),
-                box(width = 9, title = "Mapa", status = "warning", solidHeader = F, 
+                box(width = 9, title = "Map", status = "info", solidHeader = F, 
                     tags$style(type = "text/css", "#mapa {height: calc(100vh - 150px) !important;}"),
                     leafletOutput("mapa", width = "auto")
                 )
+        ),
+        tabItem(tabName = "tab2",
+                box(width = 3, title = "Select Province", status = "primary", solidHeader = TRUE,
+                    selectizeInput("selectProvincia2", "Select Province", choices = provincias$Nombre, multiple = F, selected = "Murcia"),
+                    selectizeInput("selectNacionalidad2", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad)),
+                    checkboxInput("sort", "Sort data?", value = F)
+                    ),
+                box(width = 9, title = "Chart", status = "info", solidHeader = F, 
+                    tags$style(type = "text/css", "#chart {height: calc(100vh - 150px) !important;}"),
+                    highchartOutput(outputId = "chart", width = "auto")
+                )
+                
         )
       )
     )
