429e3f1917aff81f8d984f892767951a9fb6e5fb
hackerrank <me@hackerrank.com>
2020-01-15 18:56:53 +0100

    Add shinytests, lintr and travis

--- global.R
+++ global.R
@@ -1,16 +1,16 @@
 #install.packages('shiny', 'shinydashboard', 'shinyjs', 'rgdal', 'raster', 'sp', 'pxR', 'leaflet', 'plotKML')
-
-library(shiny)
-library(shinydashboard)
-library(shinyjs)
-library(rgdal)
-library(raster) 
-library(sp)
-library(pxR)
-library(leaflet)
-library(plotKML)
-library(highcharter)
-
+suppressPackageStartupMessages({
+  library(shiny)
+  library(shinydashboard)
+  library(shinyjs)
+  library(rgdal)
+  library(raster)
+  library(sp)
+  library(pxR)
+  library(leaflet)
+  library(plotKML)
+  library(highcharter)
+})
 
 #comunidades <- read.csv("datos_csv/codccaa.csv", fileEncoding = "UTF-8")
 provincias <- read.csv("datos_csv/codprov.csv", fileEncoding = "UTF-8")
@@ -19,16 +19,16 @@ municipios <- read.csv("datos_csv/Municipios_Censo_2011.csv", fileEncoding = "UT
 secciones <- readRDS("seccionado/secciones.rds") # saveRDS(secciones, "seccionado/secciones.rds") # git lfs track ..(FILE)..
 
 # SXnacional <- as.data.frame(read.px("poblacion/2018/0003.px"))
-# 
+#
 # SXnacionalAmbos <- SXnacional[which(SXnacional$sexo == "Ambos Sexos"), ]
 # SXnacionalHombres <- SXnacional[which(SXnacional$sexo == "Hombres"), ]
 # SXnacionalMujeres <- SXnacional[which(SXnacional$sexo == "Mujeres"), ]
-# 
+#
 # saveRDS(SXnacionalAmbos, "poblacion/2018/SXnacional2018ambos.rds")
 # saveRDS(SXnacionalHombres, "poblacion/2018/SXnacional2018hombres.rds")
 # saveRDS(SXnacionalMujeres, "poblacion/2018/SXnacional2018mujeres.rds")
 
-SXnacionalAmbos <- readRDS("poblacion/2018/SXnacional2018ambos.rds") 
+SXnacionalAmbos <- readRDS("poblacion/2018/SXnacional2018ambos.rds")
 SXnacionalHombres <- readRDS("poblacion/2018/SXnacional2018hombres.rds")
 SXnacionalMujeres <- readRDS("poblacion/2018/SXnacional2018mujeres.rds")
 
--- ui.R
+++ ui.R
@@ -1,11 +1,11 @@
-dashboardPage(
+dashboardPage(skin = "blue", title = "Foreign Insight",
   dashboardHeader(title = "Foreign Insight"),
     dashboardSidebar(
       sidebarMenu(
         menuItem("Map it", tabName = "tab1", icon = shiny::icon("building")),
         menuItem("Statistics by Province", tabName = "tab2", icon = shiny::icon("brain")),
-        menuItem("Spain Map", tabName = "tab3", icon = shiny::icon("binoculars")),
-        menuItem("Historic data", tabName = "tab4", icon = shiny::icon(""))
+        menuItem("Spain Map", tabName = "tab3", icon = shiny::icon("binoculars"))#,
+        #menuItem("Historic data", tabName = "tab4", icon = shiny::icon(""))
         
     )
   ),
@@ -16,7 +16,7 @@ dashboardPage(
                 box(width = 3, title = "Census Sections", status = "primary", solidHeader = TRUE,
                     selectizeInput("selectProvincia", "Select Province", choices = provincias$Nombre, multiple = F, selected = "Murcia"),
                     selectizeInput("selectMunicipio", label = "Select Municipality", choices = c(""), multiple = T),
-                    selectizeInput("selectNacionalidad", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad)),
+                    selectizeInput("selectNacionalidad", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad), selected = "Bulgaria"),
                     checkboxInput("porcentaje", "Apply percentage", value = F),
                     checkboxInput("hombreMujer", "Distinguish man/woman", value = F),
                     useShinyjs(),
@@ -31,7 +31,7 @@ dashboardPage(
         tabItem(tabName = "tab2",
                 box(width = 2, title = "Provinces", status = "primary", solidHeader = TRUE,
                     selectizeInput("selectProvincia2", "Select Province", choices = provincias$Nombre, multiple = F, selected = "Murcia"),
-                    selectizeInput("selectNacionalidad2", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad)),
+                    selectizeInput("selectNacionalidad2", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad), selected = "Bulgaria"),
                     checkboxInput("sort", "Sort data", value = F),
                     checkboxInput("manWoman", "Men/women column", value = F),
                     checkboxInput("percentage2", "Percentage column", value = F)
@@ -44,7 +44,7 @@ dashboardPage(
         ),
         tabItem(tabName = "tab3",
                 box(width = 2, title = "Spain", status = "primary", solidHeader = TRUE,
-                    selectizeInput("selectNacionalidad3", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad))
+                    selectizeInput("selectNacionalidad3", label = "Select Nationality", choices = levels(SXnacionalAmbos$nacionalidad), selected = "Bulgaria")
                 ),
                 box(width = 10, title = "Population", status = "info", solidHeader = F, 
                     tags$style(type = "text/css", "#spainmap {height: calc(100vh - 163px) !important;}"),
