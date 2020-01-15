b5594c467d826e4303f9d0dcf1be1d4e3ae576ea
GeiserX <9169332+GeiserX@users.noreply.github.com>
2020-01-15 23:43:26 +0100

    fix path

--- tests/run_test.R
+++ tests/run_test.R
@@ -6,7 +6,7 @@ library(shinytest)
 print(getwd())
 
 test_that("Application works", {
-  expect_pass(testApp(paste0(getwd(), "tests/mytest.R"), compareImages = T))
+  expect_pass(testApp(paste0(getwd(), "/tests/mytest.R"), compareImages = T))
 })
 
 #recordTest(".")
