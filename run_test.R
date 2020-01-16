779bd3a8970328728a6854362a34b823e0a84a9b
hackerrank <me@hackerrank.com>
2020-01-16 23:08:39 +0100

    fix path

--- run_test.R
+++ run_test.R
@@ -3,11 +3,17 @@
 library(testthat)
 library(shinytest)
 
-print(getwd())
-
 test_that("Application works", {
   expect_pass(testApp(appDir = getwd(), compareImages = F))
 })
+
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("Package Style", {
+    lintr::expect_lint_free(".")
+  })
+}
+
 
 #recordTest()
 #testApp(".", "mytest")
