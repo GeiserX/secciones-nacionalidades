6b78dfbe7dd7f47409bcaa237c67a61ee0426b41
hackerrank <me@hackerrank.com>
2020-01-16 23:25:30 +0100

    fix path

--- run_test.R
+++ run_test.R
@@ -7,13 +7,6 @@ test_that("Application works", {
   expect_pass(testApp(appDir = getwd(), compareImages = F))
 })
 
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("Package Style", {
-    lintr::expect_lint_free(".")
-  })
-}
-
 
 #recordTest()
 #testApp(".", "mytest")
